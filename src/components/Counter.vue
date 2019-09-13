ac40130583d4435f5528c0e497e8e5ba3fe5eeec
khuynhthanh <phamanhtuan1406@gmail.com>
2019-09-13 17:36:48 +0700

    Master: create component counter

---
+++ src/components/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <button class="btn btn-success" @click="increment">Increment</button>
+        <button class="btn btn-success" @click="decrement">Decrement</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+            }
+        },
+        methods: {
+            increment() {
+                this.$store.state.result++;
+            },
+            decrement() {
+                this.$store.state.result--;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
