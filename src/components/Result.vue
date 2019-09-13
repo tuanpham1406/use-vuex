4a3782f0a8b2c876ed58b9d9d753dce0179405b3
khuynhthanh <phamanhtuan1406@gmail.com>
2019-09-13 17:38:36 +0700

    Master: create component result

---
+++ src/components/Result.vue
@@ -0,0 +1,17 @@
+<template>
+    <p>This is result: {{ result }}</p>
+</template>
+
+<script>
+    export default {
+        computed: {
+            result() {
+                return this.$store.state.result
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
