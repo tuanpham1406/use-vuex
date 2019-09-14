29d6ac8bb3a7dbdd87f682839c6a42c26e1fcdde
khuynhthanh <phamanhtuan1406@gmail.com>
2019-09-14 09:41:14 +0700

    Master: used map getter - getter of store

--- src/components/Other.vue
+++ src/components/Other.vue
@@ -1,13 +1,20 @@
 <template>
-    <p>This is result: {{ result }}</p>
+    <div>
+        <p>This is result: {{ tenResult }}</p>
+        <p>This is name result: {{nameResult}}</p>
+    </div>
 </template>
 
 <script>
+    import {mapGetters} from 'vuex'
+
     export default {
         computed: {
-            result() {
-                return this.$store.state.result
-            }
+            ...mapGetters(["tenResult", "nameResult"])
         }
     }
 </script>
+
+<style scoped>
+
+</style>
