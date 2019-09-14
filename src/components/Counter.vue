7907d75f0bc3926dfa9197224335837e80ccdb61
khuynhthanh <phamanhtuan1406@gmail.com>
2019-09-14 11:38:33 +0700

    Master: sử dụng v-model để bind dữ liệu 2 chiều nâng cao  với get, set - v-model

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -2,21 +2,39 @@
     <div>
         <button class="btn btn-success" @click="increment">Increment</button>
         <button class="btn btn-success" @click="asyncDecrement">Decrement</button>
+        <hr>
+        <br>
+        <input type="text" v-model="value">
+        <p>Text test: {{value}}</p>
     </div>
 </template>
 
 <script>
     import {mapActions} from 'vuex'
+
     export default {
         data() {
-            return {
+            return {}
+        },
+        computed: {
+            value: {
+                get() {
+                    return this.$store.getters.value;
+                },
+                set(value) {
+                    this.$store.dispatch('updateValue', value);
+                }
             }
         },
         methods: {
             // increment() {
             //     this.$store.commit("incrementOption", 30);
             // },
-            ...mapActions(["increment", "asyncDecrement"])
+            ...
+                mapActions(["increment", "asyncDecrement"]),
+            updateValue(event) {
+                this.$store.dispatch('updateValue', event.target.value)
+            }
         }
     }
 </script>
